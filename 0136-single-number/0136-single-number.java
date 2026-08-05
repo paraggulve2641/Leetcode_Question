f84ce219d2d0cd4a5e05fe3cad87c2c6f541a66a
Parag Harish Gulve <144191673+paraggulve2641@users.noreply.github.com>
2026-08-05 08:26:52 +0530

    Time: 532 ms (7.41%), Space: 46.5 MB (97.99%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,9 +1,18 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        int ans = 0;
-        for(int i =0 ; i<nums.length ; i++){
-            ans = ans ^ nums[i];
+        int ans[] = new int[2];
+        for(int i =0;i<nums.length;i++){
+            int count = 0;
+            for(int j = 0;j<nums.length;j++){
+                if(nums[i]==nums[j]){
+                    count++;
+                }
+            }
+             if(count==1){
+              return nums[i];    
+             }
         }
-        return ans;
+       
+        return -1;
     }
 }
