0506135fb87ac404ad72e2f7fdfcc21365f1280d
Parag Harish Gulve <144191673+paraggulve2641@users.noreply.github.com>
2026-08-08 22:51:33 +0530

    Time: 2 ms (18.86%), Space: 43.4 MB (78.21%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,15 +1,13 @@
 class Solution {
-    public void sortColors(int[] arr) {
-        int n = arr.length;
-        int noOfZeroes = 0, noOfOnes = 0;
-         for(int i = 0; i<n; i++){
-            if(arr[i] == 0) noOfZeroes++;
-            if(arr[i] == 1) noOfOnes++;
-         }
-         for(int i = 0; i<n; i++){
-            if(i<noOfZeroes) arr[i] = 0;
-            else if(i<noOfZeroes+noOfOnes) arr[i] = 1;
-            else arr[i] = 2;
-         }
+    public void sortColors(int[] nums) {
+        for(int i = 0;i<nums.length;i++){
+            for(int j=i+1;j<nums.length;j++){
+                if(nums[i]>nums[j]){
+                    int temp = nums[i];
+                    nums[i]=nums[j];
+                    nums[j]=temp;
+                }
+            }
+        }
     }
 }
